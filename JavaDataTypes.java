0973df3e833fdbad8b6582e7b81d2a5ce44f6d33
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 18:08:18 -0500

    Explicando tipos de datos primitivos

---
+++ JavaDataTypes.java
@@ -0,0 +1,21 @@
+public class JavaDataTypes {
+    public static void main (String[] args){
+	//Los tipos de datos de Java se dividen en dos grandes grupos
+	
+	//Tipo de datos primitivos : byte, short, int, long, float, double, boolean and char
+	
+	byte b;       //almacena números de -128 a 127 (1 byte)
+	short s;      //almacena números de -32768 a 32767 (2 bytes)
+	int i;        //almacena números de -2147483648 a 2147483647 (4 bytes)
+	long l;       //almacena números de -9223372036854775808 a 922337203685477580732768 (8 bytes)
+	float f;      //almacena números fraccionarios de 6 a 7 dígitos decimales (4 bytes) 
+	double d;     //almacena números fraccionarios de 15 dígitos decimales (8 bytes) 
+	boolean bo;   //almacena 2 estados (verdadero - falso) (1 byte)
+	char c;       //almacena un solo caracter (2 bytes)
+
+
+	//Tipo de datos no primitivos : String, Arrays, y Clases
+	
+	
+    }
+}
