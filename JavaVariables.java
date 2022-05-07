0c1eb056624e321ba6e388b77577e90a3ba4bf02
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 14:54:53 -0500

    Declaración y asignación de variables

--- JavaVariables.java
+++ JavaVariables.java
@@ -5,8 +5,18 @@ public class JavaVariables {
 	
 	String text = "Hello";    //almacena cadenas de texto entre comillas
 	int number = 1;           //almacena valores numéricos enteros
-	double decimal = 1.5;      //almacena valores numéricos decimales
+	double decimal = 1.5;     //almacena valores numéricos decimales
 	char caracter = 'a';      //almacena caracteres, usa comillas simples
 	boolean estado = true;    //almacena valores con dos estados (verdadero o falso)
+
+	//Declaración y asignación de variables
+	
+	//Declaración
+	int n1;
+	//Asignación
+	n1 = 5;
+
+	//También se puede declarar e inicializar directamente (como los primeros ejemplos)
+	int n2 = 10; 
     }
 }
