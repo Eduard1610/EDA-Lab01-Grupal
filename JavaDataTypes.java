02144bf72f085ca92a448793738e088c5f20de28
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 18:15:49 -0500

    Añadiendo String de tipo no primitivo

--- JavaDataTypes.java
+++ JavaDataTypes.java
@@ -7,15 +7,14 @@ public class JavaDataTypes {
 	byte b;       //almacena números de -128 a 127 (1 byte)
 	short s;      //almacena números de -32768 a 32767 (2 bytes)
 	int i;        //almacena números de -2147483648 a 2147483647 (4 bytes)
-	long l;       //almacena números de -9223372036854775808 a 922337203685477580732768 (8 bytes)
+	long l;       //almacena números de -9223372036854775808 a 922337203685477580732768, debe terminar en "L" (8 bytes)
 	float f;      //almacena números fraccionarios de 6 a 7 dígitos decimales (4 bytes) 
 	double d;     //almacena números fraccionarios de 15 dígitos decimales (8 bytes) 
 	boolean bo;   //almacena 2 estados (verdadero - falso) (1 byte)
 	char c;       //almacena un solo caracter (2 bytes)
 
-
 	//Tipo de datos no primitivos : String, Arrays, y Clases
 	
-	
+	String cadena = "Hola amigos";
     }
 }
