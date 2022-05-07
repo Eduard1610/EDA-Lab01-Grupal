fab7acb3f08e4a0834f5bdae9ce0c0c25e7f380c
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 18:50:45 -0500

    Definiendo String y ejemplos de length, toUpperCase y toLowerCase

---
+++ JavaStrings.java
@@ -0,0 +1,21 @@
+public class JavaStrings {
+    public static void main (String[] args){
+	
+	//Los Strings son usados para contener texto
+	//Debido a que es una clase predeterminada de Java contiene sus propios métodos:
+	
+	//Length (Cantidad de caracteres que contiene el String)
+	String saludo = "Hola";
+	System.out.println("La cantidad de caracteres de " + saludo + " es: " + saludo.length());
+
+	//toUpperCase y toLowerCase (Convierten todos los caracteres dentro de un String en mayúsculas y minúsculas respectivamente) 
+	String nombreUniversidad = "Universidad Nacional de San Agustin";
+	String nombreUniMayus = nombreUniversidad.toUpperCase();
+	String nombreUniMinus = nombreUniversidad.toLowerCase();
+
+	System.out.println("Normal: " + nombreUniversidad);
+	System.out.println("Mayúscula: " + nombreUniMayus);
+	System.out.println("Minúscula: " + nombreUniMinus);
+
+    }
+}
