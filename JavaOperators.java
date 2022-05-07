9ffdb6003ab73c12e56e43b7997faac2bc773719
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 18:42:01 -0500

    Definiendo operadores de java y pequño ejemplo

---
+++ JavaOperators.java
@@ -0,0 +1,21 @@
+public class JavaOperators {
+    public static void main (String[] args) {
+    
+	//Los operadores se utilizan para realizar operaciones en variables y valores
+
+	//Java divide los operadores en los siguientes grupos: 
+	//*Operadores aritméticos ( + , - , * , / , % , ++ , -- ) 
+	//*Operadores de Asignación ( = ) y sus combinaciones ( += , -= , etc ) 
+	//*Operadores de comparación ( == , != , > , < , >= , <= )
+	//*Operadores logicos ( && , || , ! )
+	//*Operadores bit a bit
+	
+	int x = 5;
+	x = x + 5;
+	System.out.println("Usando operador aritmético: " + x);
+	
+	int y = 10;
+	y += 3;
+	System.out.println("Usando operador de asignación: " + y);
+    }
+}
