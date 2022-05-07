72109d06101c4f3cea52bccc1d88c48ee282f209
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 14:51:22 -0500

    Definiendo tipos de variables

---
+++ JavaVariables.java
@@ -0,0 +1,12 @@
+public class JavaVariables {
+    public static void main (String[] args){
+
+	//Tipos de variables en Java
+	
+	String text = "Hello";    //almacena cadenas de texto entre comillas
+	int number = 1;           //almacena valores numéricos enteros
+	double decimal = 1.5;      //almacena valores numéricos decimales
+	char caracter = 'a';      //almacena caracteres, usa comillas simples
+	boolean estado = true;    //almacena valores con dos estados (verdadero o falso)
+    }
+}
