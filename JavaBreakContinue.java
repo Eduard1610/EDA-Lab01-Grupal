02185056ef787ef1eb75ef22fc9d4d84c8d3dc8b
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-08 11:31:27 -0500

    BREAK y CONTINUE: Uso de Continue en los bucles

--- JavaBreakContinue.java
+++ JavaBreakContinue.java
@@ -9,6 +9,18 @@ public class JavaBreakContinue {
         break;
       }
     }
-    
+
+    //CONTINUE : Cuando se ejecuta, detiene la ejecucion total del bloque y pasa a la siguiente iteracion
+    int k = 0;
+    while (k < 10) {
+      if (k == 4) {
+        k++;
+        continue;
+        //Cuando se llegue a la linea de codigo superior (luego de que k == 4 llege a ser TRUE)
+        //no se ejecutara el resto del bloque pasando a la siguiente iteracion
+      }
+      System.out.println(i);
+      k++;
+    }
   }
 }
