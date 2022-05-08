bf5cb3eba24f1f836e60cbd1e3fe0011db67f73d
M-UNSA <82843764+M-UNSA@users.noreply.github.com>
2022-05-08 14:28:26 -0500

    Push de archivos iniciales a branch M-UNSA

---
+++ Output.java
@@ -0,0 +1,9 @@
+public class Output {
+    public static void main(String[]args){
+        System.out.println("Java Output");
+        System.out.println("También puedes realizar operaciones matemáticas dentro de el método println");
+        System.out.println("System.out.println(3 + 3);");
+        System.out.print("\nTambién existe el método print() que es bastante similar al método println().");
+        System.out.println(" La única diferencia es que no añade ningún salto de línea.");
+    }
+}
