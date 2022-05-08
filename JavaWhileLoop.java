a007827edbe0d33a3f3804c77226c1f1633864b4
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-08 10:58:48 -0500

    Bucle WHILE: ejemplo

---
+++ JavaWhileLoop.java
@@ -0,0 +1,11 @@
+public class JavaWhileLoop {
+  public static void main (String [] args){
+    //BUCLE WHILE : Ejecuta un bloque de codigo mientras la condicion sea True
+    int i = 5;
+    while (i < 10) { //mientras que el valor de i sea menor a 10 se imprimira su valor
+      System.out.println(i);
+      i++;
+    }
+    
+  }
+}
