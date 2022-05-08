75eaa719343d23c8a68aa8b076ca60f7e055af71
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-07 19:37:27 -0500

    Ejemplos de Math.random entre 0.0 - 1.0 y un rango controlado

--- JavaMath.java
+++ JavaMath.java
@@ -21,5 +21,19 @@ public class JavaMath {
 	double numNegativo = -15.3;
 	System.out.println("Valor original: " + numNegativo);
 	System.out.println("Usando Math.abs: " + Math.abs(numNegativo));
+
+	//Math.random - Devuelve un valor entre 0.0 (incluido) y 1.0 (excluido) - valores decimales
+	double valorRandom = Math.random();
+	System.out.println("Valor random: " + valorRandom);
+
+	//Si quisiéramos un rango mejor podríamos multiplicarlo por el número máx que queremos que sea el tope del rango
+	//Y sumamos el rango mínmo
+	
+	int ranMax = 100;
+	int ranMin = 50;//Se incluye el 50
+	int randomControlado = (int) (Math.random() * 100 + 50);//casting de decimal a entero		
+	System.out.println("Valor random controlado: " + randomControlado);
+	
+
     }
 }
